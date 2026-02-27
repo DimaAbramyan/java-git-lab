f16b99cb75e489e30fa8c5b435ed88164b61e3a9
Kulmi <dabramian@sfedu.ru>
2026-02-27 14:15:38 +0300

    Realize sum

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,8 @@
 public class Utils {
     public static void main(String[] args) {
-        System.out.println("Greetings from main!");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println("Sum is " + c);
     }
 }
